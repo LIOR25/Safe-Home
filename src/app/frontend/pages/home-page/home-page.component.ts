91b2599f71873d4589bb66c5e9bfccab8db78b48
Lior <lior2512@gmail.com>
2019-11-07 15:47:34 +0200

    home page cmp user cards with img

--- src/app/frontend/pages/home-page/home-page.component.ts
+++ src/app/frontend/pages/home-page/home-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as faker from 'faker';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  resImage = faker.image.imageUrl(400, 400, 'people');
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    console.log(this.resImage);
 
+  }
 }
